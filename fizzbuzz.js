3e5624dfa6d5c8ea3ddd7995725b6fe83ba7d20e
maxthebronson <93240093+maxthebronson@users.noreply.github.com>
2024-05-31 12:46:01 -0600

    fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,27 @@
+function fizzBuzz(num) {
 
+    for (var i = 1; i <= num; i++) {
+        if (i === 3 || i === 6 || i === 9 || i === 12) {
+        console.log("Fizz");
+            continue
+        }
+    
+        else if ( i === 5 || i === 10 ) {
+            console.log("Buzz");
+            continue
+    
+        }
+
+        else if ( i === 15) {
+
+            console.log("FizzBuzz");
+            continue
+            
+        }
+         console.log(i);
+    }
+
+    return i;
+    
+
+}
